c30d36960282fa31330ef8cb9f81674b186c4742
Changbeom Choi <me@cbchoi.info>
2020-12-06 16:25:20 +0900

    Problem03

---
+++ Problem03/problem03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+void count_text(char* path)
+{
+	
+	/*
+	
+
+
+		Put your code here
+	
+
+	
+
+	*/
+
+	printf("Total %d characters are found.\n", count);
+}
+
+int main()
+{
+	count_text("./data");
+	return 0;
+}
